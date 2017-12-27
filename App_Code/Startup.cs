60935b7f369d0c5182dcbb2d0184ab48b059b43c
Vasu Sampath <magicparadigm@gmail.com>
2017-12-27 17:48:55 -0600

    Adding files for first time

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DocuSignSample.Startup))]
+namespace DocuSignSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ CertifiedDelivery.aspx.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Net;
+using System.IO;
+
+//using ServiceReference1;
+using System.Collections;
+using Newtonsoft.Json;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+
+public partial class CertifiedDeliveryExample : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            primarySignerSection.Visible = true;
+            jointSignerSection.Visible = false;
+
+            button.Visible = true;
+            uploadButton.InnerText = "Upload";
+            button.InnerText = "Submit";
+            docusignFrame.Visible = false;
+            docusignFrameIE.Visible = false;
+        }
+
+        // Add event handlers for the navigation button on each of the wizard pages 
+        PrefillClick.ServerClick += new EventHandler(prefill_Click);
+        button.ServerClick += new EventHandler(button_Click);
+        uploadButton.ServerClick += new EventHandler(uploadButton_Click);
+    }
+
+    protected void prefill_Click(object sender, EventArgs e)
+    {
+        firstname.Value = "Warren";
+        lastname.Value = "Bytendorp";
+        email.Value = "magicparadigm@live.com";
+
+    }
+
+    protected void button_Click(object sender, EventArgs e)
+    {
+        primarySignerSection.Visible = false;
+        jointSignerSection.Visible = false;
+        mainForm.Visible = false;
+        button.Visible = false;
+        createEnvelope();
+
+    }
+
+    protected void uploadButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (FileUpload1.HasFile)
+            {
+                String filename = Path.GetFileName(FileUpload1.FileName);
+                FileUpload1.SaveAs(Server.MapPath("~/App_Data/") + filename);
+                uploadFile.Value = filename;
+            }
+        }
+        catch (Exception ex)
+        {
+            uploadFile.Value = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+        }
+    }
+
+    protected String RandomizeClientUserID()
+    {
+        Random random = new Random();
+
+        return (random.Next()).ToString();
+    }
+
+    public class TextTab
+    {
+        public string tabLabel { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Tabs
+    {
+        public List<TextTab> textTabs { get; set; }
+        public List<SignHereTab> signHereTabs { get; set; }
+        public List<DateSignedTab> dateSignedTabs { get; set; }
+    }
+
+    public class CertifiedDelivery
+    {
+
+        public string email { get; set; }
+        public string name { get; set; }
+        public int recipientId { get; set; }
+        public string roleName { get; set; }
+        public string routingOrder { get; set; }
+        public string clientUserId { get; set; }
+
+
+    }
+
+    public class Recipients
+    {
+        public List<CertifiedDelivery> certifiedDeliveries { get; set; }
+    }
+
+    public class Document
+    {
+        public string documentId { get; set; }
+        public string name { get; set; }
+        public string transformPdfFields { get; set; }
+
+    }
+
+    public class InlineTemplate
+    {
+        public string sequence { get; set; }
+        public Recipients recipients { get; set; }
+
+        public List<Document> documents { get; set; }
+    }
+
+    public class ServerTemplate
+    {
+        public string sequence { get; set; }
+        public string templateId { get; set; }
+    }
+
+    public class CompositeTemplate
+    {
+        public string compositeTemplateId { get; set; }
+        public List<InlineTemplate> inlineTemplates { get; set; }
+        public List<ServerTemplate> serverTemplates { get; set; }
+        public Document document { get; set; }
+    }
+
+    public class CreateEnvelopeRequest
+    {
+        public string status { get; set; }
+        public string emailSubject { get; set; }
+        public string emailBlurb { get; set; }
+        public List<CompositeTemplate> compositeTemplates { get; set; }
+    }
+    public class CreateEnvelopeResponse
+    {
+        public string envelopeId { get; set; }
+        public string uri { get; set; }
+        public string statusDateTime { get; set; }
+        public string status { get; set; }
+    }
+
+    public class SignHereTab
+    {
+        public string tabId { get; set; }
+        public string name { get; set; }
+        public string pageNumber { get; set; }
+        public string documentId { get; set; }
+        public string yPosition { get; set; }
+        public string xPosition { get; set; }
+
+        public string anchorString { get; set; }
+        public string anchorXOffset { get; set; }
+        public string anchorYOffset { get; set; }
+        public string anchorIgnoreIfNotPresent { get; set; }
+        public string anchorUnits { get; set; }
+    }
+
+    public class DateSignedTab
+    {
+        public string tabId { get; set; }
+        public string name { get; set; }
+        public string pageNumber { get; set; }
+        public string documentId { get; set; }
+        public string yPosition { get; set; }
+        public string xPosition { get; set; }
+        public string anchorString { get; set; }
+        public string anchorXOffset { get; set; }
+        public string anchorYOffset { get; set; }
+        public string anchorIgnoreIfNotPresent { get; set; }
+        public string anchorUnits { get; set; }
+
+    }
+
+
+    private static void WriteStream(Stream reqStream, string str)
+    {
+        byte[] reqBytes = UTF8Encoding.UTF8.GetBytes(str);
+        reqStream.Write(reqBytes, 0, reqBytes.Length);
+    }
+
+
+    private String GetSecurityHeader()
+    {
+        String str = "";
+        str = "<DocuSignCredentials>" + "<Username>" + ConfigurationManager.AppSettings["API.Email"] + "</Username>" +
+            "<Password>" + ConfigurationManager.AppSettings["API.Password"] + "</Password>" +
+            "<IntegratorKey>" + ConfigurationManager.AppSettings["API.IntegratorKey"] + "</IntegratorKey>" +
+            "</DocuSignCredentials>";
+        return str;
+    }
+
+    public class RecipientViewRequest
+    {
+        public string authenticationMethod { get; set; }
+        public string email { get; set; }
+        public string returnUrl { get; set; }
+        public string userName { get; set; }
+        public string clientUserId { get; set; }
+    }
+
+
+    public class RecipientViewResponse
+    {
+        public string url { get; set; }
+    }
+
+    protected void createEnvelope()
+    {
+
+
+        // Set up the envelope
+        CreateEnvelopeRequest createEnvelopeRequest = new CreateEnvelopeRequest();
+        createEnvelopeRequest.emailSubject = "Certified Delivery Example";
+        createEnvelopeRequest.status = "sent";
+        createEnvelopeRequest.emailBlurb = "Example of how certified delivery works";
+
+        // Define first signer 
+        CertifiedDelivery signer = new CertifiedDelivery();
+        signer.email = email.Value;
+        signer.name = firstname.Value + " " + lastname.Value;
+        signer.recipientId = 1;
+        signer.routingOrder = "1";
+        signer.roleName = "Signer1";
+ //       signer.clientUserId = RandomizeClientUserID();  // First signer is embedded 
+
+        // Define a document 
+        Document document = new Document();
+        document.documentId = "1";
+        document.name = "Sample Form";
+//        document.transformPdfFields = "true";
+
+        // Define an inline template
+        InlineTemplate inline1 = new InlineTemplate();
+        inline1.sequence = "2";
+        inline1.recipients = new Recipients();
+        inline1.recipients.certifiedDeliveries = new List<CertifiedDelivery>();
+        inline1.recipients.certifiedDeliveries.Add(signer);
+
+
+        // Add the inline template to a CompositeTemplate 
+        CompositeTemplate compositeTemplate1 = new CompositeTemplate();
+        compositeTemplate1.inlineTemplates = new List<InlineTemplate>();
+        compositeTemplate1.inlineTemplates.Add(inline1);
+        compositeTemplate1.document = document;
+
+        // Add compositeTemplate to the envelope 
+        createEnvelopeRequest.compositeTemplates = new List<CompositeTemplate>();
+        createEnvelopeRequest.compositeTemplates.Add(compositeTemplate1);
+
+
+        string output = JsonConvert.SerializeObject(createEnvelopeRequest);
+
+        // Specify a unique boundary string that doesn't appear in the json or document bytes.
+        string Boundary = "MY_BOUNDARY";
+
+        // Set the URI
+        HttpWebRequest request = HttpWebRequest.Create(ConfigurationManager.AppSettings["DocuSignServer"] + "/restapi/v2/accounts/" + ConfigurationManager.AppSettings["API.AccountID"] + "/envelopes") as HttpWebRequest;
+
+        // Set the method
+        request.Method = "POST";
+
+        // Set the authentication header
+        request.Headers["X-DocuSign-Authentication"] = GetSecurityHeader();
+
+        // Set the overall request content type aand boundary string
+        request.ContentType = "multipart/form-data; boundary=" + Boundary;
+        request.Accept = "application/json";
+
+        // Start forming the body of the request
+        Stream reqStream = request.GetRequestStream();
+
+        // write boundary marker between parts
+        WriteStream(reqStream, "\n--" + Boundary + "\n");
+
+        // write out the json envelope definition part
+        WriteStream(reqStream, "Content-Type: application/json\n");
+        WriteStream(reqStream, "Content-Disposition: form-data\n");
+        WriteStream(reqStream, "\n"); // requires an empty line between the header and the json body
+        WriteStream(reqStream, output);
+
+        // write out the form bytes for the first form
+        WriteStream(reqStream, "\n--" + Boundary + "\n");
+        WriteStream(reqStream, "Content-Type: application/pdf\n");
+        WriteStream(reqStream, "Content-Disposition: file; filename=\"Sample_Form\"; documentId=1\n");
+        WriteStream(reqStream, "\n");
+        String filename = uploadFile.Value;
+        if (File.Exists(Server.MapPath("~/App_Data/" + filename)))
+        {
+            // Read the file contents and write them to the request stream
+            byte[] buf = new byte[4096];
+            int len;
+            // read contents of document into the request stream
+            FileStream fileStream = File.OpenRead(Server.MapPath("~/App_Data/" + filename));
+            while ((len = fileStream.Read(buf, 0, 4096)) > 0)
+            {
+                reqStream.Write(buf, 0, len);
+            }
+            fileStream.Close();
+        }
+
+
+        // wrte the end boundary marker - ensure that it is on its own line
+        WriteStream(reqStream, "\n--" + Boundary + "--");
+        WriteStream(reqStream, "\n");
+
+        try
+        {
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                byte[] responseBytes = new byte[response.ContentLength];
+                using (var reader = new System.IO.BinaryReader(response.GetResponseStream()))
+                {
+                    reader.Read(responseBytes, 0, responseBytes.Length);
+                }
+                string responseText = Encoding.UTF8.GetString(responseBytes);
+                CreateEnvelopeResponse createEnvelopeResponse = new CreateEnvelopeResponse();
+
+                createEnvelopeResponse = JsonConvert.DeserializeObject<CreateEnvelopeResponse>(responseText);
+                if (createEnvelopeResponse.status.Equals("sent"))
+                {
+                    Response.Redirect("ConfirmationPage.aspx?envelopeID=" + createEnvelopeResponse.envelopeId);
+                }
+            }
+        }
+        catch (WebException ex)
+        {
+            if (ex.Status == WebExceptionStatus.ProtocolError)
+            {
+                HttpWebResponse response = (HttpWebResponse)ex.Response;
+                using (var reader = new System.IO.StreamReader(ex.Response.GetResponseStream(), UTF8Encoding.UTF8))
+                {
+                    string errorMess = reader.ReadToEnd();
+                    log4net.ILog logger = log4net.LogManager.GetLogger(typeof(CertifiedDelivery));
+                    logger.Info("\n----------------------------------------\n");
+                    logger.Error("DocuSign Error: " + errorMess);
+                    logger.Error(ex.StackTrace);
+                    Response.Write(ex.Message);
+                }
+            }
+            else
+            {
+                log4net.ILog logger = log4net.LogManager.GetLogger(typeof(CertifiedDelivery));
+                logger.Info("\n----------------------------------------\n");
+                logger.Error("WebRequest Error: " + ex.Message);
+                logger.Error(ex.StackTrace);
+                Response.Write(ex.Message);
+            }
+        }
+
+    }
+}
